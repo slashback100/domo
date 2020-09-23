031680f3acd30f1518c809cd76e9b36dbc35e394
slashback100 <christophe.desirant@gmail.com>
2020-09-23 13:42:30 +0200

    properties

--- tools/switchLight.sh
+++ tools/switchLight.sh
@@ -2,4 +2,4 @@
 . ../modules/properties
 light=light."$1"
 action="$2"
-curl -s -X POST -H "Content-Type: application/json" -H "X-HA-Access: $password" -d '{"entity_id": "'$light'"}'  "http://localhost:8123/api/services/light/turn_$action" > /dev/null
+curl -s -X POST -H "Content-Type: application/json" -H "X-HA-Access: $apiPassword" -d '{"entity_id": "'$light'"}'  "http://localhost:8123/api/services/light/turn_$action" > /dev/null
